23a0bf5f9dd7e0127c020625bb8fefe965a871c6
Patrick Malatesta <patrick.malatesta@ucdconnect.ie>
2019-01-25 16:33:53 +0000

    name update

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
-setup(name="systeminformation",
+setup(name="system_information",
       version="1.0",
       description="Basic system information",
       url="",
       author="Patrick Malatesta",
       author_email="patrick.malatesta@ucdconnect.ie",
       license="GPL3",
-      packages=['systeminformation'],
+      packages=['system_information'],
       entry_points={
-          'console_scripts':['systeminformation=systeminformation.main:main']
+          'console_scripts':['system_information=system_information.main:main']
           }
       )
